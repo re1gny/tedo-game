08e45952615a7e2bb3aecc7798fda16d59c818e8
re1gny <ya.manshin@mail.ru>
2024-10-28 01:04:30 +0300

    add images

--- src/constants/screens.js
+++ src/constants/screens.js
@@ -1,3 +1,134 @@
+import Start1BackImage from '../assets/images/start1/back.png';
+import Start1PanelBackImage from '../assets/images/start1/panelBack.png';
+import Start2BackImage from '../assets/images/start2/back.png';
+import Start2PanelBackImage from '../assets/images/start2/panelBack.png';
+import Level1BackImage from '../assets/images/level1/back.png';
+import Level1IntroBackImage from '../assets/images/level1Intro/back.png';
+import Level1IntroPanelBackImage from '../assets/images/level1Intro/panelBack.png';
+import Level1RulesPanelBackImage from '../assets/images/level1Rules/panelBack.png';
+import Level1RulesPersonImage from '../assets/images/level1Rules/person.svg';
+import Level1RulesTaskImage from '../assets/images/level1Rules/task.svg';
+import Level1RulesLine1Image from '../assets/images/level1Rules/lines/line1.svg';
+import Level1RulesLine2Image from '../assets/images/level1Rules/lines/line2.svg';
+import Level1RulesLine3Image from '../assets/images/level1Rules/lines/line3.svg';
+import Level1RulesLine4Image from '../assets/images/level1Rules/lines/line4.svg';
+import Level1RulesSubline1Image from '../assets/images/level1Rules/sublines/subline1.svg';
+import Level1RulesSubline2Image from '../assets/images/level1Rules/sublines/subline2.svg';
+import Level1RulesSubline3Image from '../assets/images/level1Rules/sublines/subline3.svg';
+import Level1RulesSubline4Image from '../assets/images/level1Rules/sublines/subline4.svg';
+import Level1GamePanelBackImage from '../assets/images/level1Game/panelBack.png';
+import Level1GamePerson1Image from '../assets/images/level1Game/person1.svg';
+import Level1GamePerson2Image from '../assets/images/level1Game/person2.svg';
+import Level1GamePerson3Image from '../assets/images/level1Game/person3.svg';
+import Level1GameTask1Image from '../assets/images/level1Game/task1.svg';
+import Level1GameTask2Image from '../assets/images/level1Game/task2.svg';
+import Level1GameTask3Image from '../assets/images/level1Game/task3.svg';
+import Level1GamePerson1Line1Image from '../assets/images/level1Game/lines/person1Line1.svg';
+import Level1GamePerson1Line2Image from '../assets/images/level1Game/lines/person1Line2.svg';
+import Level1GamePerson1Line3Image from '../assets/images/level1Game/lines/person1Line3.svg';
+import Level1GamePerson1Line4Image from '../assets/images/level1Game/lines/person1Line4.svg';
+import Level1GamePerson1Line5Image from '../assets/images/level1Game/lines/person1Line5.svg';
+import Level1GamePerson1Line6Image from '../assets/images/level1Game/lines/person1Line6.svg';
+import Level1GamePerson1Line7Image from '../assets/images/level1Game/lines/person1Line7.svg';
+import Level1GamePerson1Line8Image from '../assets/images/level1Game/lines/person1Line8.svg';
+import Level1GamePerson1Line9Image from '../assets/images/level1Game/lines/person1Line9.svg';
+import Level1GamePerson2Line1Image from '../assets/images/level1Game/lines/person2Line1.svg';
+import Level1GamePerson2Line2Image from '../assets/images/level1Game/lines/person2Line2.svg';
+import Level1GamePerson2Line3Image from '../assets/images/level1Game/lines/person2Line3.svg';
+import Level1GamePerson2Line4Image from '../assets/images/level1Game/lines/person2Line4.svg';
+import Level1GamePerson2Line5Image from '../assets/images/level1Game/lines/person2Line5.svg';
+import Level1GamePerson2Line6Image from '../assets/images/level1Game/lines/person2Line6.svg';
+import Level1GamePerson2Line7Image from '../assets/images/level1Game/lines/person2Line7.svg';
+import Level1GamePerson2Line8Image from '../assets/images/level1Game/lines/person2Line8.svg';
+import Level1GamePerson2Line9Image from '../assets/images/level1Game/lines/person2Line9.svg';
+import Level1GamePerson2Line10Image from '../assets/images/level1Game/lines/person2Line10.svg';
+import Level1GamePerson2Line11Image from '../assets/images/level1Game/lines/person2Line11.svg';
+import Level1GamePerson3Line1Image from '../assets/images/level1Game/lines/person3Line1.svg';
+import Level1GamePerson3Line2Image from '../assets/images/level1Game/lines/person3Line2.svg';
+import Level1GamePerson3Line3Image from '../assets/images/level1Game/lines/person3Line3.svg';
+import Level1GamePerson3Line4Image from '../assets/images/level1Game/lines/person3Line4.svg';
+import Level1GamePerson3Line5Image from '../assets/images/level1Game/lines/person3Line5.svg';
+import Level1GamePerson3Line6Image from '../assets/images/level1Game/lines/person3Line6.svg';
+import Level1GamePerson3Line7Image from '../assets/images/level1Game/lines/person3Line7.svg';
+import Level1GamePerson3Line8Image from '../assets/images/level1Game/lines/person3Line8.svg';
+import Level1GamePerson3Line9Image from '../assets/images/level1Game/lines/person3Line9.svg';
+import Level1GamePerson3Line10Image from '../assets/images/level1Game/lines/person3Line10.svg';
+import Level1GamePerson1Subline1Image from '../assets/images/level1Game/sublines/person1Subline1.svg';
+import Level1GamePerson1Subline2Image from '../assets/images/level1Game/sublines/person1Subline2.svg';
+import Level1GamePerson1Subline3Image from '../assets/images/level1Game/sublines/person1Subline3.svg';
+import Level1GamePerson1Subline4Image from '../assets/images/level1Game/sublines/person1Subline4.svg';
+import Level1GamePerson1Subline5Image from '../assets/images/level1Game/sublines/person1Subline5.svg';
+import Level1GamePerson1Subline6Image from '../assets/images/level1Game/sublines/person1Subline6.svg';
+import Level1GamePerson1Subline7Image from '../assets/images/level1Game/sublines/person1Subline7.svg';
+import Level1GamePerson1Subline8Image from '../assets/images/level1Game/sublines/person1Subline8.svg';
+import Level1GamePerson1Subline9Image from '../assets/images/level1Game/sublines/person1Subline9.svg';
+import Level1GamePerson1Subline10Image from '../assets/images/level1Game/sublines/person1Subline10.svg';
+import Level1GamePerson2Subline1Image from '../assets/images/level1Game/sublines/person2Subline1.svg';
+import Level1GamePerson2Subline2Image from '../assets/images/level1Game/sublines/person2Subline2.svg';
+import Level1GamePerson2Subline3Image from '../assets/images/level1Game/sublines/person2Subline3.svg';
+import Level1GamePerson2Subline4Image from '../assets/images/level1Game/sublines/person2Subline4.svg';
+import Level1GamePerson2Subline5Image from '../assets/images/level1Game/sublines/person2Subline5.svg';
+import Level1GamePerson2Subline6Image from '../assets/images/level1Game/sublines/person2Subline6.svg';
+import Level1GamePerson2Subline7Image from '../assets/images/level1Game/sublines/person2Subline7.svg';
+import Level1GamePerson2Subline8Image from '../assets/images/level1Game/sublines/person2Subline8.svg';
+import Level1GamePerson2Subline9Image from '../assets/images/level1Game/sublines/person2Subline9.svg';
+import Level1GamePerson2Subline10Image from '../assets/images/level1Game/sublines/person2Subline10.svg';
+import Level1GamePerson2Subline11Image from '../assets/images/level1Game/sublines/person2Subline11.svg';
+import Level1GamePerson2Subline12Image from '../assets/images/level1Game/sublines/person2Subline12.svg';
+import Level1GamePerson3Subline1Image from '../assets/images/level1Game/sublines/person3Subline1.svg';
+import Level1GamePerson3Subline2Image from '../assets/images/level1Game/sublines/person3Subline2.svg';
+import Level1GamePerson3Subline3Image from '../assets/images/level1Game/sublines/person3Subline3.svg';
+import Level1GamePerson3Subline4Image from '../assets/images/level1Game/sublines/person3Subline4.svg';
+import Level1GamePerson3Subline5Image from '../assets/images/level1Game/sublines/person3Subline5.svg';
+import Level1GamePerson3Subline6Image from '../assets/images/level1Game/sublines/person3Subline6.svg';
+import Level1GamePerson3Subline7Image from '../assets/images/level1Game/sublines/person3Subline7.svg';
+import Level1GamePerson3Subline8Image from '../assets/images/level1Game/sublines/person3Subline8.svg';
+import Level1GamePerson3Subline9Image from '../assets/images/level1Game/sublines/person3Subline9.svg';
+import Level1GamePerson3Subline10Image from '../assets/images/level1Game/sublines/person3Subline10.svg';
+import Level1GamePerson3Subline11Image from '../assets/images/level1Game/sublines/person3Subline11.svg';
+import Level1FinalBackImage from '../assets/images/level1Final/back.png';
+import Level1FinalPanelBackImage from '../assets/images/level1Final/panelBack.png';
+import Level2BackImage from '../assets/images/level2/back.png';
+import Level2PuzzleBackHorizontalImage from '../assets/images/level2/puzzleBackHorizontal.png';
+import Level2PuzzleBackVerticalImage from '../assets/images/level2/puzzleBackVertical.png';
+import Level2IntroBackImage from '../assets/images/level2Intro/back.png';
+import Level2IntroPanelBackImage from '../assets/images/level2Intro/panelBack.png';
+import Level2RulesPanelBackImage from '../assets/images/level2Rules/panelBack.png';
+import Level2RulesPuzzlesBackImage from '../assets/images/level2Rules/puzzlesBack.png';
+import Level2RulesPuzzle1Image from '../assets/images/level2Rules/puzzles/puzzle1.png';
+import Level2RulesPuzzle2Image from '../assets/images/level2Rules/puzzles/puzzle2.png';
+import Level2RulesPuzzlePlace1Image from '../assets/images/level2Rules/puzzlePlaces/puzzlePlace1.svg';
+import Level2RulesPuzzlePlace2Image from '../assets/images/level2Rules/puzzlePlaces/puzzlePlace2.svg';
+import Level2GameLightImage from '../assets/images/level2Game/light.png';
+import Level2GameModalBackImage from '../assets/images/level2Game/modalBack.png';
+import Level2GamePuzzlesBackImage from '../assets/images/level2Game/puzzlesBack.png';
+import Level2GamePuzzle1Image from '../assets/images/level2Game/puzzles/puzzle1.png';
+import Level2GamePuzzle2Image from '../assets/images/level2Game/puzzles/puzzle2.png';
+import Level2GamePuzzle3Image from '../assets/images/level2Game/puzzles/puzzle3.png';
+import Level2GamePuzzle4Image from '../assets/images/level2Game/puzzles/puzzle4.png';
+import Level2GamePuzzle5Image from '../assets/images/level2Game/puzzles/puzzle5.png';
+import Level2GamePuzzle6Image from '../assets/images/level2Game/puzzles/puzzle6.png';
+import Level2GamePuzzle7Image from '../assets/images/level2Game/puzzles/puzzle7.png';
+import Level2GamePuzzle8Image from '../assets/images/level2Game/puzzles/puzzle8.png';
+import Level2GamePuzzlePlace1Image from '../assets/images/level2Game/puzzlePlaces/puzzlePlace1.svg';
+import Level2GamePuzzlePlace2Image from '../assets/images/level2Game/puzzlePlaces/puzzlePlace2.svg';
+import Level2GamePuzzlePlace3Image from '../assets/images/level2Game/puzzlePlaces/puzzlePlace3.svg';
+import Level2GamePuzzlePlace4Image from '../assets/images/level2Game/puzzlePlaces/puzzlePlace4.svg';
+import Level2GamePuzzlePlace5Image from '../assets/images/level2Game/puzzlePlaces/puzzlePlace5.svg';
+import Level2GamePuzzlePlace6Image from '../assets/images/level2Game/puzzlePlaces/puzzlePlace6.svg';
+import Level2GamePuzzlePlace7Image from '../assets/images/level2Game/puzzlePlaces/puzzlePlace7.svg';
+import Level2GamePuzzlePlace8Image from '../assets/images/level2Game/puzzlePlaces/puzzlePlace8.svg';
+import Level2FinalBackImage from '../assets/images/level2Final/back.png';
+import Level2FinalPanelBackImage from '../assets/images/level2Final/panelBack.png';
+import Level3Back1Image from '../assets/images/level3/back1.png';
+import Level3Back2Image from '../assets/images/level3/back2.png';
+import Level3IntroPanelBackImage from '../assets/images/level3Intro/panelBack.png';
+import Level3RulesPanelBackImage from '../assets/images/level3Rules/panelBack.png';
+import Level3GamePanelBackImage from '../assets/images/level3Game/panelBack.png';
+import Level3FinalPanelBackImage from '../assets/images/level3Final/panelBack.png';
+import FinalBackImage from '../assets/images/final/back.png';
+import FinalPanelBackImage from '../assets/images/final/panelBack.png';
+
 export const SCREEN_NAME = {
     Start1: 'START_1',
     Start2: 'START_2',
@@ -35,19 +166,153 @@ export const NEXT_SCREEN = {
 };
 
 export const SCREEN_IMAGES = {
-    [SCREEN_NAME.Start1]: [],
-    [SCREEN_NAME.Start2]: [],
-    [SCREEN_NAME.Leve1Intro]: [],
-    [SCREEN_NAME.Leve1Rules]: [],
-    [SCREEN_NAME.Leve1Game]: [],
-    [SCREEN_NAME.Leve1Final]: [],
-    [SCREEN_NAME.Leve2Intro]: [],
-    [SCREEN_NAME.Leve2Rules]: [],
-    [SCREEN_NAME.Leve2Game]: [],
-    [SCREEN_NAME.Leve2Final]: [],
-    [SCREEN_NAME.Leve3Intro]: [],
-    [SCREEN_NAME.Leve3Rules]: [],
-    [SCREEN_NAME.Leve3Game]: [],
-    [SCREEN_NAME.Leve3Final]: [],
-    [SCREEN_NAME.Final]: [],
+    [SCREEN_NAME.Start1]: [Start1BackImage, Start1PanelBackImage],
+    [SCREEN_NAME.Start2]: [Start2BackImage, Start2PanelBackImage],
+    [SCREEN_NAME.Leve1Intro]: [Level1IntroBackImage, Level1IntroPanelBackImage],
+    [SCREEN_NAME.Leve1Rules]: [
+        Level1BackImage,
+        Level1RulesPanelBackImage,
+        Level1RulesPersonImage,
+        Level1RulesTaskImage,
+        Level1RulesLine1Image,
+        Level1RulesLine2Image,
+        Level1RulesLine3Image,
+        Level1RulesLine4Image,
+        Level1RulesSubline1Image,
+        Level1RulesSubline2Image,
+        Level1RulesSubline3Image,
+        Level1RulesSubline4Image,
+    ],
+    [SCREEN_NAME.Leve1Game]: [
+        Level1BackImage,
+        Level1GamePanelBackImage,
+        Level1GamePerson1Image,
+        Level1GamePerson2Image,
+        Level1GamePerson3Image,
+        Level1GameTask1Image,
+        Level1GameTask2Image,
+        Level1GameTask3Image,
+        Level1GamePerson1Line1Image,
+        Level1GamePerson1Line2Image,
+        Level1GamePerson1Line3Image,
+        Level1GamePerson1Line4Image,
+        Level1GamePerson1Line5Image,
+        Level1GamePerson1Line6Image,
+        Level1GamePerson1Line7Image,
+        Level1GamePerson1Line8Image,
+        Level1GamePerson1Line9Image,
+        Level1GamePerson2Line1Image,
+        Level1GamePerson2Line2Image,
+        Level1GamePerson2Line3Image,
+        Level1GamePerson2Line4Image,
+        Level1GamePerson2Line5Image,
+        Level1GamePerson2Line6Image,
+        Level1GamePerson2Line7Image,
+        Level1GamePerson2Line8Image,
+        Level1GamePerson2Line9Image,
+        Level1GamePerson2Line10Image,
+        Level1GamePerson2Line11Image,
+        Level1GamePerson3Line1Image,
+        Level1GamePerson3Line2Image,
+        Level1GamePerson3Line3Image,
+        Level1GamePerson3Line4Image,
+        Level1GamePerson3Line5Image,
+        Level1GamePerson3Line6Image,
+        Level1GamePerson3Line7Image,
+        Level1GamePerson3Line8Image,
+        Level1GamePerson3Line9Image,
+        Level1GamePerson3Line10Image,
+        Level1GamePerson1Subline1Image,
+        Level1GamePerson1Subline2Image,
+        Level1GamePerson1Subline3Image,
+        Level1GamePerson1Subline4Image,
+        Level1GamePerson1Subline5Image,
+        Level1GamePerson1Subline6Image,
+        Level1GamePerson1Subline7Image,
+        Level1GamePerson1Subline8Image,
+        Level1GamePerson1Subline9Image,
+        Level1GamePerson1Subline10Image,
+        Level1GamePerson2Subline1Image,
+        Level1GamePerson2Subline2Image,
+        Level1GamePerson2Subline3Image,
+        Level1GamePerson2Subline4Image,
+        Level1GamePerson2Subline5Image,
+        Level1GamePerson2Subline6Image,
+        Level1GamePerson2Subline7Image,
+        Level1GamePerson2Subline8Image,
+        Level1GamePerson2Subline9Image,
+        Level1GamePerson2Subline10Image,
+        Level1GamePerson2Subline11Image,
+        Level1GamePerson2Subline12Image,
+        Level1GamePerson3Subline1Image,
+        Level1GamePerson3Subline2Image,
+        Level1GamePerson3Subline3Image,
+        Level1GamePerson3Subline4Image,
+        Level1GamePerson3Subline5Image,
+        Level1GamePerson3Subline6Image,
+        Level1GamePerson3Subline7Image,
+        Level1GamePerson3Subline8Image,
+        Level1GamePerson3Subline9Image,
+        Level1GamePerson3Subline10Image,
+        Level1GamePerson3Subline11Image,
+    ],
+    [SCREEN_NAME.Leve1Final]: [Level1FinalBackImage, Level1FinalPanelBackImage],
+    [SCREEN_NAME.Leve2Intro]: [Level2IntroBackImage, Level2IntroPanelBackImage],
+    [SCREEN_NAME.Leve2Rules]: [
+        Level2BackImage,
+        Level2PuzzleBackHorizontalImage,
+        Level2PuzzleBackVerticalImage,
+        Level2RulesPanelBackImage,
+        Level2RulesPuzzlesBackImage,
+        Level2RulesPuzzle1Image,
+        Level2RulesPuzzle2Image,
+        Level2RulesPuzzlePlace1Image,
+        Level2RulesPuzzlePlace2Image,
+    ],
+    [SCREEN_NAME.Leve2Game]: [
+        Level2BackImage,
+        Level2PuzzleBackHorizontalImage,
+        Level2PuzzleBackVerticalImage,
+        Level2GameLightImage,
+        Level2GameModalBackImage,
+        Level2GamePuzzlesBackImage,
+        Level2GamePuzzle1Image,
+        Level2GamePuzzle2Image,
+        Level2GamePuzzle3Image,
+        Level2GamePuzzle4Image,
+        Level2GamePuzzle5Image,
+        Level2GamePuzzle6Image,
+        Level2GamePuzzle7Image,
+        Level2GamePuzzle8Image,
+        Level2GamePuzzlePlace1Image,
+        Level2GamePuzzlePlace2Image,
+        Level2GamePuzzlePlace3Image,
+        Level2GamePuzzlePlace4Image,
+        Level2GamePuzzlePlace5Image,
+        Level2GamePuzzlePlace6Image,
+        Level2GamePuzzlePlace7Image,
+        Level2GamePuzzlePlace8Image,
+    ],
+    [SCREEN_NAME.Leve2Final]: [Level2FinalBackImage, Level2FinalPanelBackImage],
+    [SCREEN_NAME.Leve3Intro]: [
+        Level3Back1Image,
+        Level3Back2Image,
+        Level3IntroPanelBackImage,
+    ],
+    [SCREEN_NAME.Leve3Rules]: [
+        Level3Back1Image,
+        Level3Back2Image,
+        Level3RulesPanelBackImage,
+    ],
+    [SCREEN_NAME.Leve3Game]: [
+        Level3Back1Image,
+        Level3Back2Image,
+        Level3GamePanelBackImage,
+    ],
+    [SCREEN_NAME.Leve3Final]: [
+        Level3Back1Image,
+        Level3Back2Image,
+        Level3FinalPanelBackImage,
+    ],
+    [SCREEN_NAME.Final]: [FinalBackImage, FinalPanelBackImage],
 };
